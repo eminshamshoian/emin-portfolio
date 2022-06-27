c1be3dbea725e0c193814e955f3e7ef9724a6815
eminshamshoian <47337592+eminshamshoian@users.noreply.github.com>
2022-06-27 16:14:07 -0700

    added prt project

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -29,6 +29,9 @@ const Portfolio = () => {
                 design is responsive to all screen sizes.
               </p>
               <p>
+                <strong>Development Role</strong>: Built Entire Application
+              </p>
+              <p>
                 <strong>Tech Used:</strong> HTML, CSS, JavaScript, Typescript,
                 React, Nextjs, Tailwindcss, Sanity Studio, Vercel.
               </p>
@@ -43,6 +46,51 @@ const Portfolio = () => {
                 </a>
                 <a
                   href='https://github.com/eminshamshoian/global-chronicles'
+                  className='main-color text-decoration-underline mx-4'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Code
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className='col-md-6 col-lg-4'>
+            <div className='portfolio-item'>
+              <img
+                src='p7.png'
+                className='w-100 img-thumbnail'
+                alt='portfolio item'
+              />
+              <h3 className='text-capitalize fs-5 my-2'>
+                SwiftifyTicketing - Classroom Ticketing
+              </h3>
+              <p>
+                Built for senior capstone project at CSUN university, this
+                applicaiton is designed to track classroom issues and provide a
+                central ticketing dashboard for users. The application is made
+                for professors to report issues found in class and repair
+                faculty to track and fix them while keeping progress.
+              </p>
+              <p>
+                <strong>Development Role</strong>: Full Stack | Built Entire
+                Backend
+              </p>
+              <p>
+                <strong>Tech Used:</strong> HTML, CSS, JavaScript, Typescript,
+                React, Nodejs.
+              </p>
+              <p className='mb-4'>
+                <a
+                  href='https://gleaming-snickerdoodle-22026b.netlify.app/sign-in'
+                  className='main-color text-decoration-underline'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Demo
+                </a>
+                <a
+                  href='https://github.com/swiftifyticketing'
                   className='main-color text-decoration-underline mx-4'
                   target='_blank'
                   rel='noopener noreferrer'
